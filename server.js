1d42f5f9732629060fbda0ed0a487bc3c4874c6c
Vatsal Sanchala <98870768+bevatsal1122@users.noreply.github.com>
2022-07-31 01:30:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 const connectDB = require('./config/database');
