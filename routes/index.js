af763daa34abc6bdc9ff616d1084734600b086e2
Vatsal Sanchala <98870768+bevatsal1122@users.noreply.github.com>
2022-08-02 15:40:12 +0530

    Crucial Updates

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { fileController, showFilesController, downloadController, sendEmailController } = require('../controllers');
+const { uploadController, showFilesController, downloadController, sendEmailController } = require('../controllers');
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('home');
 })
-router.post('/api/files/upload', fileController.upload);
+
+router.post('/api/files/upload', uploadController.upload);
 router.get('/files/:uuid', showFilesController.show);
 router.get('/files/download/:uuid', downloadController.download);
 router.post('/api/files/sendemail', sendEmailController.sendemail);
